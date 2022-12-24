ef8ae35016934a30b9282fa6b67e85d594a93a81
Anatolii <100527802+AnatolyKuzmin@users.noreply.github.com>
2022-12-24 18:56:39 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+  /* рандом */
+  var Beer = ["Портер (Porter)", "Стаут ​​(Stout)", "Коричневый (Brown Ale)", "Пейл (Pale Ale)", "Горький (Bitter)", 
+        "Пшеничный (Weizen, Weiss)", "Белый (Weisse)", 
+        "Мягкий (Mild)", "Ячменное вино (Barley Wine)", "Светлый лагер (Light lager)", 
+        "Пилзнер (Pilsner)", "Европейский янтарный лагер", "Ледовое пиво (Ice beer)", 
+        "Темный лагер (Dark lager)", "Драфт (Draft / Draught)", "Бок (Bock)", "Копченый Лагер (Rauch)", 
+        "Ламбик (Lambic)", "«Старое пиво» (Альтбир)", "Кёльши (Kolsch)"];
+  var output = Beer[Math.floor(Math.random() * 20)];
+  
+  document.getElementById('button').onclick = function() {
+    let field1 = document.getElementById('field');
+    let out = '';
+        out += `<p class="alert" role="alert">${output}</p>`;
+        field1.innerHTML = out;
+}
